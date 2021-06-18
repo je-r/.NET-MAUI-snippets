94481ea2cb00cd7dd77de4c0604ab968b81f0e85
J.E. R <5433476+je-r@users.noreply.github.com>
2021-06-18 19:56:13 +0200

    Add files via upload

---
+++ gui1.cs
@@ -0,0 +1,10 @@
+readonly State count = 0;
+[Body]
+View body() => new StackLayout
+{
+    new Label("Welcome to .NET MAUI!"),
+    new Button(
+        () => $"You clicked {count} times.",
+        () => count.Value ++)
+    )
+};
